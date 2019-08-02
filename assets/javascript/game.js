55c3482b9dea82f25bd6f0c1cba491517ef2ee68
Anthony McPherson <aqm28202@gmail.com>
2019-08-02 09:29:06 -0400

    <psuedo coding game.js and added first array>

---
+++ assets/javascript/game.js
@@ -0,0 +1,2 @@
+// Create an array that lists out all of the alphabet options for user to guess. 
+var LetterOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
