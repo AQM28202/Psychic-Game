ce3159848fdf49dbfc830a9f2c94b96d11fd540f
Anthony McPherson <aqm28202@gmail.com>
2019-08-02 10:19:31 -0400

    <establishing intial variables and functions for user guesses>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,13 @@
 // Create an array that lists out all of the alphabet options for user to guess. 
-var LetterOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letterOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+
+// Determines which key was pressed.
+var userGuess = event.key;
+
+// Randomly chooses a letter from letterOptions array. This is the Computer's guess.
+var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
+
+}
