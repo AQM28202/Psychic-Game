4835e9bc427425220fe909a9d6b47eb890ae5ce5
Anthony McPherson <aqm28202@gmail.com>
2019-08-04 11:30:27 -0400

    <rearraging code so from top-dowm so some things fire later than others>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,12 @@ var losses = 0;
 var guessesLeft = 9;
 var guessList = []
 
+// Create variables that hold references to the places in the HTML where we want to display things.
+var userChoiceText = document.getElementById("guessessofar-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesleft-text");
+
 
 // Randomly chooses a letter from letterOptions array. This is the Computer's guess.
 var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
@@ -25,20 +31,19 @@ guessesLeft--;
 guessList.push(userGuess);
 console.log("Guesses: " + guessList);
 
+if (userGuess === computerGuess) {
+    wins++;
+    guessesLeft = 9;
+    guessList = []
 
+}
 
+else if (guessesLeft === 0) {
+    guessesLeft = 9;
+    attemptList = [];
+    losses++;
 
-
-
-
-// Create variables that hold references to the places in the HTML where we want to display things.
-var userChoiceText = document.getElementById("guessessofar-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guessesleft-text");
-
-
-
+}
 // Displays the user guess, wins, losses, and guesses left
 userChoiceText.textContent = "Your Guesses So Far: " + guessList;
 winsText.textContent = "Wins: " + wins;
@@ -46,20 +51,17 @@ lossesText.textContent = "Losses: " + losses;
 guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
 
 
+}
+
+
+
+
+
+
 
 // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
 
 
-if (userGuess !== computerGuess && guessesLeft !=0) {
-      guessesLeft--;
-    } 
-if (userGuess !== computerGuess && guessesLeft === 0) {
-        losses++; guessesLeft=9;
-    }
-if (userGuess === computerGuess) {
-        wins++; guessesLeft=9;
-    }
-}
 
 
 // 1. How do I get all 9 user guesses to print? 
